45916553df94488cf49892eee0275d68956a97aa
Jeffrey Cordero <jeffreycordero@outlook.com>
2018-03-10 23:11:23 +0000

    screw github and aws
    really just frontend dev needs to die in hell

--- frontend/webhook.js
+++ frontend/webhook.js
@@ -39,12 +39,44 @@ function sendMessage(event) {
 
 	if(aiText === REQUEST_NEED) {
 	    // GET https://graph.facebook.com/v2.6/<USER_ID>?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=<PAGE_ACCESS_TOKEN>
-	    console.log('Log 1: ', 'REQUEST_NEED')
+	    request({
+				url: 'https://graph.facebook.com/v2.6/${event.sender.id}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=${TOKEN}',
+				qs: {access_token: Token},
+				method: 'GET'
+		}, (error, response) => {
+				if (error) {
+			console.log('Error sending message: ', error);
+				} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+				}
+		});
+		
+		console.log(request.body)
+		
+		// Parse body and put to db
+		
+		console.log('Log 1: ', 'REQUEST_NEED')
 	}
 	
 	if(aiText === REQUEST_RESPOND) {
 	    // GET https://graph.facebook.com/v2.6/<USER_ID>?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=<PAGE_ACCESS_TOKEN>
-	    console.log('Log 2: ', 'REQUEST_RESPOND')
+	    request({
+				url: 'https://graph.facebook.com/v2.6/${event.sender.id}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=${TOKEN}',
+				qs: {access_token: Token},
+				method: 'GET'
+		}, (error, response) => {
+				if (error) {
+			console.log('Error sending message: ', error);
+				} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+				}
+		});
+		
+		console.log(request.body)
+		
+		// Parse body and put to db
+		
+		console.log('Log 2: ', 'REQUEST_RESPOND')
 	}
 	
 	request({
@@ -75,38 +107,30 @@ function sendMessage(event) {
 const http = require('http')
 const Bot = require('messenger-bot')
 const Token = 'EAACceGmDZCd4BAA2OqbE5CWuLfJi8rtrZB3lCoFZBzJMwZB7KZC5l8rxzBGVsr9uEREs1JtY6gc2Y4fHkEqfyPV3mlKnqGcsfodrwvtUhWiGGsTZAGwTxT9SWka23V8dc4RFDjzccPXbwQx3e4lf09zfqHc2CgJvXmT55aY3RJuAZDZD'
-
 let bot = new Bot({
   token: Token,
   verify: 'tuxedo_cat',
   app_secret: ''
 })
-
 /*
 const NEED_INTENT = 'register_need';  // the action name from the Dialogflow intent
 const REGISTER_INTENT = 'register_responder';  // the action name from the Dialogflow intent
-
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 const app = new DialogflowApp({request: bot.request, response: bot.response});
-
 let actionMap = new Map();
 actionMap.set(NEED_INTENT, handleRegisterNeed);
 actionMap.set(REGISTER_INTENT, handleRegisterResponder);
 app.handleRequest(actionMap);
-
 function handleRegisterNeed(app){
     app.tell('Handling Registering Need...');
 }
-
 function handleRegisterResponder(app) {
     app.tell('Handling Registering Need...');
 }
 *
-
 bot.on('error', (err) => {
     console.log("FUCK!", err.message)
 })
-
 bot.on('message', (payload, reply) => {
     let text = sendMessage(payload)
     bot.getProfile(payload.sender.id, (err, profile) => {
@@ -117,7 +141,6 @@ bot.on('message', (payload, reply) => {
 	})
     })
 })
-
 http.createServer(bot.middleware()).listen(5000)
 console.log('Echo bot server running at port 5000.')
 */
